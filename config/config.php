e7c1dd68ced6472c569c8bbf3c0040b2722e4b52
AmaNnanna <talk2nally@gmail.com>
2023-05-11 17:10:02 +0100

    git

---
+++ config/config.php
@@ -0,0 +1,59 @@
+<?
+
+define("appname","fordbank");
+define('app_title','');
+define('appid','1');
+define("version","2.0.0");
+define("debug",true);
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+define('DIR', __DIR__);
+define("display_error",true);
+define("language","en_US");
+define("url",__DIR__);
+define("baseurl",__DIR__);
+define("apps_dir","./_apps/");
+define("templates_dir","./templates/");
+define("templates_default","404");
+define("templates_default_route","/error/404/");
+define("vendor_dir","./vendor/");
+define("assets_dir","./templates/assets");
+define("admin_assets_dir","./templates/admin/assets");
+define("layouts_dir","./templates/layouts/");
+define("template_file_extension","php");
+define("store_dir","./_store/");
+define("public_dir","./_public/");
+define("plugins_dir","./templates/_plugins/");
+define("server","remote");
+define("use_token_security",true);
+define("encrypt_salt","7WAO342QFANY6IKBF7RERRGERT9388239WL3VMT920VB5NQMW");
+define("default_timezone","Africa/Lagos");
+define("offset_timezone",true);
+define("session_path","./_sessions/");
+define("session_timout",20);
+define("session_delete_timout",30);
+define("auth_session_key","logged_in");
+define("auth_url","/ibanking/login");
+define("domain", "https://landmarkfinanceonline.com/");
+define("enable_DKIM_keys",false);
+
+define("site_use_key","d485e0c84b4c555d707f2815dc7c94fb593ccbf9");
+
+define("db_host","localhost");
+define("db_user","hpvpbwip_iujix");
+define("db_password","3s2W^Fu%2@VgPxpC%B!tpx");
+define("db","hpvpbwip_iujix");
+define("db_port",null);
+define("db_charset","utf8");
+define("db_socket",null);
+
+define("start_accid",6809800000);
+define("transfer_redirect_time",10);
+define("show_currency_market",true);
+
+define("PHPMAILER_IS_HTML",false);
+define("fromEmail","noReply@landmarkfinanceonline.com");
+define("fromName","LandMark Finance");
+define("replyEmail","noReply@landmarkfinanceonline.com");
+define("replyName","LandMark Finance");
+define("subject","LandMark Finance");
